3e4933519cd7ac898787217cf2e7d0d3d9dac974
ahmad1091 <ahmadtayeb91@gmail.com>
2021-03-28 22:05:21 +0300

    modify the value and the amountOutMin to hex

--- index.js
+++ index.js
@@ -34,9 +34,13 @@ const init = async () => {
   const to = "0x86bEF5C3c0c3C79bB5aEa65FDf1AdE4BA5ADe6E5";
   const deadline = Math.floor(Date.now() / 1000) + 60 * 20;
   const value = trade.inputAmount.raw;
+  const inputAmountHex = ethers.BigNumber.from(value.toString()).toHexString();
+  const amountOutMinHex = ethers.BigNumber.from(
+    amountOutMin.toString()
+  ).toHexString();
 
-  const provider = ethers.getDefaultProvider("mainnet", {
-    infura: "https://mainnet.infura.io/v3/1977e1bd2cbe44ed875e37e9e4ba0260",
+  const provider = ethers.getDefaultProvider("rinkeby", {
+    infura: "https://rinkeby.infura.io/v3/1977e1bd2cbe44ed875e37e9e4ba0260",
   });
   //   let provider = new HDWalletProvider({
   //     mnemonic: {
@@ -46,26 +50,34 @@ const init = async () => {
   //       "https://rinkeby.infura.io/v3/73b626ff98a04927af975ae0a4a4a019",
   //   });
   //   const web3 = new Web3(provider);
-  const signer = new ethers.Wallet("8c5f3088b4b6465f8762c508a2cfbdfa");
+  const signer = new ethers.Wallet.fromMnemonic(
+    "carbon tower feel armed margin furnace nothing false course nature glance coyote"
+  );
   const account = signer.connect(provider);
   const uniswap = new ethers.Contract(
     "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
     [
-      "function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts);",
+      "function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts)",
     ],
     account
   );
+  gasPrice = await provider.getGasPrice();
   //   const uniswap = await new web3.eth.Contract(abi);
-  const tx = await uniswap.sendExactEthForTokens(
-    amountOutMin,
+  const tx = await uniswap.swapExactETHForTokens(
+    amountOutMinHex,
     path,
     to,
     deadline,
-    { value, gasPrice: 20e9 }
+    {
+      value: inputAmountHex,
+      gasPrice: gasPrice.toHexString(),
+      gasLimit: ethers.BigNumber.from(300000).toHexString(),
+    }
   );
+
   console.log("TX hash", tx.hash);
   const receipt = await tx.wait();
-  console.log(receipt);
+  console.log(`Transaction was mined in block ${receipt.blockNumber}`);
 };
 
 init();
